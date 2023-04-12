63239d2c717874ed49b283761d760f06ec87e7e5
ledgerluminary55 <ledgerluminary55@gmail.com>
2023-04-12 17:15:32 +0100

    add commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std] 
 use gstd::debug;
 use::gstd::{msg, prelude::*};
+//use gtest::{Log, Program, System};
 
 #[no_mangle]
 extern "C" fn handle() {
@@ -12,11 +13,11 @@ extern "C" fn init() {
     let init_message = String::from_utf8(msg::load_bytes().expect("Can't load init message")).expect("Can't decode to String");
     debug!("Program was initialized with message {:?}", init_message);
 }
-
+/* 
 #[test]
 fn hello_test() {
     let sys = System::new();
     sys.init_logger();
     let program = Program::current(&sys);
     program.send_bytes(2, String::from("INIT MESSAGE"));
-}
+}*/
--- tests/hello_world_test.rs
+++ tests/hello_world_test.rs
@@ -3,6 +3,7 @@ use gtest::{Log, Program, System};
 #[test]
 fn hello_test() {
     let sys = System::new();
-    let program = Program::from_file(&sys, "./target/wasm32-unknown-unknown/release/hello_world.wasm");
+    sys.init_logger();
     let program = Program::current(&sys);
+    program.send_bytes(2, String::from("INIT MESSAGE"));
 }
